2021313ce6c9aef39950853416df68247a3986b6
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-23 17:26:14 -0600

    Stubbed out SDServiceDiscovery

--- ServiceDiscovery/SDServiceDiscovery.cs
+++ ServiceDiscovery/SDServiceDiscovery.cs
@@ -2,10 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace ServiceDiscovery
 {
     public class SDServiceDiscovery
     {
+        private static TraceSource logger = new TraceSource("SDServiceDiscovery");
+
+        private bool isDisposed = false;
+
+        ~SDServiceDiscovery()
+        {
+            Dispose(false);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (!isDisposed && disposing)
+            {
+                // Code to dispose the managed resources of the class
+                stop();
+            }
+            // Code to dispose the un-managed resources of the class
+            isDisposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+            
+        public void stop()
+        {
+            stopSearching();
+        }
+
+        public void stopSearching()
+        {
+            logger.TraceEvent(TraceEventType.Stop, 0, "stopSearching");
+        }
+
+        public void searchForServices()
+        {
+        }
     }
 }
