abbac129a566e0e93b024ddb1b1196b5510f9c04
Arlo Louis O'Keeffe <mailtoarlo@gmail.com>
2012-11-09 16:21:26 -0700

    Added IAServer

---
+++ IntAirAct/IAServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntAirAct
+{
+    interface IAServer
+    {
+        ushort Port { get; set; }
+
+        void Start();
+
+        void Stop();
+
+        void Route(IARoute route, Action<IARequest, IAResponse> action);
+    }
+}
