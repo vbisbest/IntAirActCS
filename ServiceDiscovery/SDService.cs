16cf9bd5c1578b77aac1a56656d8bb3c883831e9
Arlo Louis O'Keeffe <mailtoarlo@gmail.com>
2012-11-09 19:45:47 -0700

    Add SDService

---
+++ ServiceDiscovery/SDService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceDiscovery
+{
+    public class SDService
+    {
+        public String Name { get; private set; }
+        public String Hostname { get; private set; }
+        public ushort Port { get; private set; }
+        public String Type { get; private set; }
+        public Dictionary<String, byte[]> TXTRecord { get; private set; }
+    }
+}
