274e3fc2237d183fa5a555af38695712d9c8aafd
Arlo Louis O'Keeffe <mailtoarlo@gmail.com>
2012-11-14 14:17:32 -0700

    Added IARequestExtensions

---
+++ IntAirAct/Model/RequestExtensions/IARequestExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntAirAct
+{
+    public static class IARequestExtensions
+    {
+        public static string BodyAsString(this IARequest request)
+        {
+            return System.Text.Encoding.UTF8.GetString(request.Body);
+        }
+        
+    }
+}
