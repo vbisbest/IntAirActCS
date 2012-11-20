ac72f773b08d9694551d93591f5388834e134e94
Arlo Louis O'Keeffe <mailtoarlo@gmail.com>
2012-11-19 19:53:10 -0700

    Implement IAModelWithStringProperty

--- IntAirActTests/Model/IAModelWithStringProperty.cs
+++ IntAirActTests/Model/IAModelWithStringProperty.cs
@@ -7,5 +7,34 @@ namespace IntAirActTests
 {
     class IAModelWithStringProperty
     {
+        public string StringProperty { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("IAModelWithStringProperty[StringProperty: {0}]", StringProperty);
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (this == obj)
+            {
+                return true;
+            }
+
+            if (obj == null || (obj as IAModelWithStringProperty) == null)
+            {
+                return false;
+            }
+
+            IAModelWithStringProperty model = (IAModelWithStringProperty)obj;
+            return (this.StringProperty != null && this.StringProperty.Equals(model.StringProperty));
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 2;
+            hash = hash * 31 + this.StringProperty.GetHashCode();
+            return hash;
+        }
     }
 }
